e6afe9c8b7ec8160ff7394d526c272641e2ca223
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2024-01-31 09:19:47 -0300

    atualizado

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
+  const name = 'Matheus'
+
+  const url = 'https://images.pexels.com/photos/19809164/pexels-photo-19809164/free-photo-of-arte-modelo-de-moda-modelo-de-beleza-preto.jpeg'
   return (
     <div className="App">
-      <h1> Olá React</h1> 
-      <p>Esse é meu primeiro passo com React</p>
+      <h1> Alterando JSX</h1> 
+      <p>Olá, {name}</p>
+      <img src={url} alt='Minha Imagem' />
+      <HelloWorld/>
     </div>
   );
 }
